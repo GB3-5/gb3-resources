17ec37b58b7dbc8cabb8f3482724dad9a750ce62
dhg26 <77782159+dhg26@users.noreply.github.com>
2023-05-26 10:46:36 +0100

    clock changed for mod_pro

--- mat_mul/matmul.c
+++ mat_mul/matmul.c
@@ -23,7 +23,7 @@
 //   return 0;
 // }
 
-#define SIZE 10
+#define SIZE 5
 void matrixMultiplication(int matrix1[SIZE][SIZE], int matrix2[SIZE][SIZE], int result[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
@@ -44,29 +44,19 @@ int main() {
     *gDebugLedsMemoryMappedRegister = 0xFF;
 
     int matrix1[SIZE][SIZE] = {
-        {9, 3, 5, 0, 2, 3, 5, 6, 4, 8},
-        {1, 3, 4, 2, 0, 7, 1, 5, 6, 9},
-        {4, 8, 2, 6, 4, 7, 3, 0, 9, 2},
-        {6, 1, 3, 8, 2, 1, 7, 4, 6, 5},
-        {5, 0, 3, 2, 9, 6, 5, 1, 4, 7},
-        {2, 8, 7, 4, 6, 0, 3, 5, 9, 1},
-        {4, 6, 2, 7, 3, 9, 8, 1, 5, 0},
-        {1, 5, 4, 9, 2, 6, 7, 3, 8, 0},
-        {7, 3, 6, 1, 5, 0, 2, 4, 9, 8},
-        {8, 9, 1, 4, 3, 5, 0, 6, 7, 2}
+        {9, 3, 5, 0, 2},
+        {1, 3, 4, 2, 0},
+        {4, 8, 2, 6, 4},
+        {6, 1, 3, 8, 2},
+        {5, 0, 3, 2, 9},
     };
 
     int matrix2[SIZE][SIZE] = {
-        {2, 4, 9, 1, 7, 8, 5, 0, 3, 6},
-        {3, 0, 6, 2, 1, 5, 4, 7, 9, 8},
-        {1, 5, 3, 8, 9, 7, 0, 4, 6, 2},
-        {8, 9, 2, 6, 4, 0, 7, 5, 3, 1},
-        {0, 7, 1, 3, 6, 9, 4, 2, 5, 8},
-        {6, 2, 8, 4, 5, 1, 3, 9, 7, 0},
-        {5, 3, 9, 0, 2, 4, 1, 6, 8, 7},
-        {9, 1, 5, 7, 3, 6, 8, 2, 0, 4},
-        {4, 6, 7, 5, 8, 3, 2, 1, 0, 9},
-        {7, 2, 4, 1, 6, 5, 3, 8, 0, 9}
+        {2, 4, 9, 1, 7},
+        {3, 0, 6, 2, 1},
+        {1, 5, 3, 8, 9},
+        {8, 9, 2, 6, 4},
+        {0, 7, 1, 3, 6},
     };
 
     int result[SIZE][SIZE];
@@ -74,8 +64,10 @@ int main() {
     
     matrixMultiplication(matrix1, matrix2, result);
     *gDebugLedsMemoryMappedRegister = ~(*gDebugLedsMemoryMappedRegister);
-    matrixMultiplication(matrix2, matrix1, result);
-    *gDebugLedsMemoryMappedRegister = ~(*gDebugLedsMemoryMappedRegister);
-
+    
+    for (int k = 0; k < 100000; k++) {
+        //Null statement to waste time
+        ;
+    }
     return 0;
 }
