193fd1b96ef5617f7634ea01a0be97a2e814f46b
dhg26 <77782159+dhg26@users.noreply.github.com>
2023-06-07 16:21:30 +0100

    backup

--- branch_test/branch_test.c
+++ branch_test/branch_test.c
@@ -1,45 +1,73 @@
+
+#define TAKEN 1
+#define NOT_TAKEN 0
 #define STRONGLY_TAKEN 3
 #define WEAKLY_TAKEN 2
-#define WEAKLY_NOT_TAKEN 1
 #define STRONGLY_NOT_TAKEN 0
+#define WEAKLY_NOT_TAKEN 1
+
+int testFunction(int a, int b) {
+    int result = 0;
+    if (a > b) {
+        result = a - b;
+    } else {
+        result = b - a;
+    }
+    return result;
+}
+
+void testLocalBranchPredictor() {
+    int i;
+    int a = 5;
+    int b = 10;
+    int result = 0;
+    int counter[2] = {WEAKLY_TAKEN, WEAKLY_NOT_TAKEN};
+
+    for (i = 0; i < 100000; i++) {
+        if (counter[i % 2] >= STRONGLY_TAKEN) {
+            result += testFunction(a, b);
+            counter[i % 2]--;
+        } else if (counter[i % 2] <= STRONGLY_NOT_TAKEN) {
+            result += testFunction(b, a);
+            counter[i % 2]++;
+        } else if (counter[i % 2] == WEAKLY_TAKEN) {
+            result += testFunction(a, b);
+            counter[i % 2]--;
+        } else if (counter[i % 2] == WEAKLY_NOT_TAKEN) {
+            result += testFunction(b, a);
+            counter[i % 2]++;
+        }
+    }
+}
+
+void test_pattern () { 
+    int sum1 = 0;
+    int sum2 = 0;
+    int sum3 = 0;
+    for (int i = 0; i < 10000; i++) {
+        if (i % 2 == 0) {
+            sum1 += i;
+        }
+
+        if (i % 3 == 0) {
+            sum2 += i;
+        } 
+
+        if (i % 5 == 0) {
+            sum3 -= i;
+        }
+    }
+}
 
 int main() {
-    int i;
-    int j;
-    int sum = 0;
-    int counter = STRONGLY_TAKEN;  // Initial counter value
     volatile unsigned int *gDebugLedsMemoryMappedRegister = (unsigned int *)0x2000;
     *gDebugLedsMemoryMappedRegister = 0xFF;
-
-    for (i = 0; i < 50000; i++) {
-        //printf("Iteration %d\n", i);
-        
-        if (counter >= WEAKLY_TAKEN) {
-            // This condition causes weakly taken behavior
-            //printf("Branch taken\n");
-            counter++;  // Increment counter
-        } else {
-            // This condition causes weakly not taken behavior
-            //printf("Branch not taken\n");
-            counter--;  // Decrement counter
-        }
-        
-        // Additional non-obvious conditional statements
-        if (i % 100 == 0) {
-            // This conditional statement flips the branch predictor from weakly taken to weakly not taken
-            if (counter >= WEAKLY_TAKEN)
-                counter = STRONGLY_NOT_TAKEN;
-            else
-                counter = STRONGLY_TAKEN;
-        }
-        
-        //printf("\n");
-    }
+    test_pattern();
+    testLocalBranchPredictor();
     *gDebugLedsMemoryMappedRegister = ~(*gDebugLedsMemoryMappedRegister);
-
-    for (j = 0; j < 1000000; j++) {
-        ;
-  }
-    
+    for (int j = 0; j < 1000000; j++) {
+    //Null statement to waste time
+    ;
+    }
     return 0;
 }
